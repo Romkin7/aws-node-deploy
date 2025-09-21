09820ab665a5a998d3bc0ffc098840c0ca32d5f8
Roman Tuomisto <romantuomisto@Mac.lan>
2025-09-22 00:07:22 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import pino from "pino";
+
+const app = express();
+const logger = pino({ level: process.env.LOG_LEVEL || "info" });
+
+app.get("/", (req, res) => {
+  logger.info("Received request for /");
+  res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
+});
